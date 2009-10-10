35fcd501211802538fc8284c94f25eeb70fc90f8
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-10 02:22:22 +0000

    slight rearrangement

--- demos/svgImport/src/testApp.cpp
+++ demos/svgImport/src/testApp.cpp
@@ -10,6 +10,7 @@ void testApp::update(){
 
 void testApp::draw(){
 	ofTranslate(svg.getAnchor().x, svg.getAnchor().y);
-	ofRotate(ofMap(mouseX, 0, ofGetWidth(), -180, 180));
 	svg.draw();
+	ofRotate(ofMap(mouseX, 0, ofGetWidth(), -180, 180));
+	svg.draw(0, 0, mouseX, mouseY);
 }
