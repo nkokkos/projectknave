9729f393fe3b172ae8d377f13ff3dbeed5e798c7
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-10 02:23:25 +0000

    added display list use + draw(x, y, width, height)

--- knaveAddons/ofxSvg/src/ofxSvg.h
+++ knaveAddons/ofxSvg/src/ofxSvg.h
@@ -24,7 +24,20 @@ protected:
 	float width, height;
 	ofxPoint2f anchor;
 	vector<ofxSvgPath> paths;
+
+	GLuint displayList;
+	bool displayListReady;
 public:
+	ofxSvg() {
+		displayList = glGenLists(1);
+		displayListReady = false;
+	}
+	~ofxSvg() {
+		glDeleteLists(displayList, 1);
+		for(int i = 0; i < size(); i++)
+			for(int j = 0; j < paths[i].size(); j++)
+				delete paths[i].commands[j];
+	}
 	void loadFile(string filename) {
 		ofxXmlSettings xml;
 		xml.loadFile(filename);
@@ -97,14 +110,34 @@ public:
 	ofxPoint2f getAnchor() {
 		return anchor;
 	}
-	void draw() const {
+	void draw(float x, float y) {
+		draw(x, y, width, height);
+	}
+	void draw(float x, float y, float width, float height) {
 		ofPushMatrix();
-		ofPushStyle();
-		ofTranslate(-anchor.x, -anchor.y);
-		for(int i = 0; i < size(); i++)
-			paths[i].draw();
-		ofPopStyle();
+		ofTranslate(x, y);
+		ofScale(width / this->width, height / this->height);
+		draw();
 		ofPopMatrix();
+	}
+	void draw() {
+		if(displayListReady) {
+			glCallList(displayList);
+		} else {
+			glNewList(displayList, GL_COMPILE);
+			ofPushMatrix();
+			ofPushStyle();
+			ofTranslate(-anchor.x, -anchor.y);
+			for(int i = 0; i < size(); i++)
+				paths[i].draw();
+			ofPopStyle();
+			ofPopMatrix();
+			glEndList();
+			displayListReady = true;
+		}
+	}
+	void update() {
+		displayListReady = false;
 	}
 	void add(ofxSvgPath& path) {
 		paths.push_back(path);
@@ -113,10 +146,5 @@ public:
 		for(int i = 0; i < svg.size(); i++)
 			out << "[" << svg.paths[i] << "]";
 		return out;
-	}
-	~ofxSvg() {
-		for(int i = 0; i < size(); i++)
-			for(int j = 0; j < paths[i].size(); j++)
-				delete paths[i].commands[j];
 	}
 };
