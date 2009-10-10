0fdf2320993f229508da73033d777173564ce676
vanderlin <vanderlin@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-10 00:35:30 +0000

    starting to build some plants

--- trunk/development/centralApp/src/scenes/TreeScene/TreeScene.cpp
+++ trunk/development/centralApp/src/scenes/TreeScene/TreeScene.cpp
@@ -9,25 +9,51 @@ void TreeScene::setup() {
 // ------------------------------------------
 
 void TreeScene::mousePressed(int x, int y, int button) {
-
+	
+	stems[curStem].initStem(x, y);
+	
 	curStem ++;
 	curStem %= MAX_STEMS;
 }
 
 // ------------------------------------------
 void TreeScene::update() {
+	
 	for(int i=0; i<MAX_STEMS; i++) {
-		stems[i].update();
+		
+		if(stems[i].bALive) {
+			
+			for(int j=0; j<building.shapes.size(); j++) {
+				for(int k=0; k<building.shapes[j].pnts.size(); k++) {
+					
+					ofxVec2f cntPnt(building.shapes[j].pnts[k].x, building.shapes[j].pnts[k].y);
+					
+					//void particle::addAttractionForce(float x, float y, float radius, float scale){
+
+					stems[i].head.addRepulsionForce(cntPnt.x, 
+													cntPnt.y,
+													30.0,
+													3.0);
+					
+					
+				}
+			}
+			
+			stems[i].update();
+		}
 	}
 }
 
 // ------------------------------------------
 void TreeScene::draw() {
 	
+	
+	// draw the building
+	building.drawContour();
+	
 	for(int i=0; i<MAX_STEMS; i++) {
 		stems[i].draw();
 	}	
 	
 	
-
 }
--- trunk/development/centralApp/src/scenes/TreeScene/TreeScene.h
+++ trunk/development/centralApp/src/scenes/TreeScene/TreeScene.h
@@ -1,20 +1,11 @@
-/*
- *  simpleScene.h
- *  openFrameworks
- *
- *  Created by Zach Lieberman on 9/23/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
 
 #pragma once;
 
 #include "ofMain.h"
 #include "baseScene.h"
 
+#include "TreeConstants.h"
 #include "Stem.h"
-
-#define MAX_STEMS 10
 
 
 class TreeScene : public baseScene {
