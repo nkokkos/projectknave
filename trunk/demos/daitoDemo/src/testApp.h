235d897c48944da1e353f956bcf73bb52e3e24d1
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-11 23:11:12 +0000

    cleaner

--- trunk/demos/daitoDemo/src/testApp.h
+++ trunk/demos/daitoDemo/src/testApp.h
@@ -14,12 +14,6 @@ class testApp : public ofBaseApp{
 		void draw();
 
 		void keyPressed  (int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-	private:
-		ofxOscSender sender;
 };
 
 #endif
