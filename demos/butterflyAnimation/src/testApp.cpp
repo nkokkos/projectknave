a31d74abb3c001ba396146b166d60ffc1680ce65
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-11 23:06:39 +0000

    ofxDaito created and uploaded, slight revision to butterflies.

--- demos/butterflyAnimation/src/testApp.cpp
+++ demos/butterflyAnimation/src/testApp.cpp
@@ -18,8 +18,10 @@ void testApp::draw(){
 	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
 	ofRotate(ofMap(mouseY, 0, ofGetHeight(), 180, 0), 1, 0, 0);
 	ofRotate(ofMap(mouseX, 0, ofGetWidth(), 90, -90), 0, 0, 1);
+	glEnable(GL_DEPTH_TEST);
 	for(int i = 0; i < butterflies.size(); i++)
 		butterflies[i].draw(ofGetElapsedTimef());
+	glDisable(GL_DEPTH_TEST);
 }
 
 void testApp::keyPressed(int key) {
