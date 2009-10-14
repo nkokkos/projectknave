affe1ff32c37f854dd89549f85cbfff7c8cab84f
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-14 13:41:11 +0000

    using plain text instead of binary serialization...

--- knaveAddons/ofxFileSharing/src/ofxFileSharingClient.h
+++ knaveAddons/ofxFileSharing/src/ofxFileSharingClient.h
@@ -11,21 +11,27 @@ protected:
 	ofxTCPClient tcpClient;
 	T packet;
 	long lastAttempt;
-	static const int reconnectTime = 500;
+	int reconnectTime;
+	bool verbose;
 public:
 	ofxFileSharingClient() {
+		reconnectTime = 500;
+		verbose = true;
 		lastAttempt = ofGetElapsedTimeMillis();
 	}
 	void setup(string filename, string ip, int port) {
+		if(verbose)
+			cout << "ofxFileSharingClient: connecting to " << ip << ":" << port << "/" << filename << endl;
 		this->filename = filename;
 		tcpClient.setup(ip, port);
 	}
 	bool update() {
 		if(tcpClient.isConnected()) {
 			if(tcpClient.receive() == "new") {
-				ifstream file(filename.c_str(), ios::out | ios::binary);
+				ifstream file(filename.c_str(), ios::in | ios::binary);
 				if(!file.is_open()) {
-					cout << "ofxFileSharingClient couldn't open shared file '" << filename << "'" << endl;
+					if(verbose)
+						cout << "ofxFileSharingClient: couldn't open shared file '" << filename << "'" << endl;
 				} else {
 					T curPacket;
 					file >> curPacket;
@@ -37,6 +43,8 @@ public:
 		} else {
 			long curTime = ofGetElapsedTimeMillis();
 			if(curTime - lastAttempt > reconnectTime) {
+				if(verbose)
+					cout << "ofxFileSharingClient: reconnecting to " << tcpClient.getIP() << ":" << tcpClient.getPort() << endl;
 				tcpClient.setup(tcpClient.getIP(), tcpClient.getPort());
 				lastAttempt = curTime;
 			}
--- knaveAddons/ofxFileSharing/src/ofxFileSharingServer.h
+++ knaveAddons/ofxFileSharing/src/ofxFileSharingServer.h
@@ -9,17 +9,22 @@ class ofxFileSharingServer {
 protected:
 	string filename;
 	ofxTCPServer tcpServer;
+	bool verbose;
 public:
 	ofxFileSharingServer() {
+		verbose = true;
 	}
 	void setup(string filename, int port) {
+		if(verbose)
+			cout << "ofxFileSharingServer: connecting to " << port << "/" << filename << endl;
 		this->filename = filename;
 		tcpServer.setup(port);
 	}
 	void send(T& packet) {
 		ofstream file(filename.c_str(), ios::out | ios::binary);
 		if(!file.is_open()) {
-			cout << "ofxFileSharingServer couldn't open shared file '" << filename << "'" << endl;
+			if(verbose)
+				cout << "ofxFileSharingServer: couldn't open shared file '" << filename << "'" << endl;
 		} else {
 			file << packet;
 			file.close();
