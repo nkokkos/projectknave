578a71b9cf2d8e7fccf2a9885cc1bbd7e7cbef80
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-14 04:20:12 +0000

    computerVisionPacket is now a class, and has a constructor that initializes its variables

--- trunk/development/centralApp/src/computerVision/cvPacket.h
+++ trunk/development/centralApp/src/computerVision/cvPacket.h
@@ -1,37 +1,40 @@
 #ifndef _CV_PACKET_H
 #define _CV_PACKET_H
 
-
 #include "ofMain.h"
 #include "ofxCvMain.h"
-
 
 #define MAX_N_BLOBS					20
 #define MAX_BLOB_LENGTH				384
 
-// this is a packet which repreents computer vision info for this given frame. 
+// this is a packet which repreents computer vision info for this given frame.
 
-typedef struct {
-	
+class computerVisionPacket {
+public:
 
 	int			myId;			// which client am I?		1 = hands, 2 = people left 3 = people right, 4 = stars
 	int			frameNumber;	// which frame am I -- for sanity.
-	
+
 	int			width;			// width and height of this video frame
-	int			height;		
-	
+	int			height;
+
 	int			nBlobs;
 	int			nPts[MAX_N_BLOBS];
-	ofPoint		pts[MAX_N_BLOBS][MAX_BLOB_LENGTH];					// todo: OF POINT IS HEAVY HERE... let's do simpler & better. 
-	
+	ofPoint		pts[MAX_N_BLOBS][MAX_BLOB_LENGTH];					// todo: OF POINT IS HEAVY HERE... let's do simpler & better.
+
 	bool		bAmInner[MAX_N_BLOBS];
 	// add more stuff here....
-	
-	int			checkSum;		// myId + frameNumber + nBlobs	?  that's not ideal but all I can think of at the moment. 
-	
-}	computerVisionPacket;
 
+	int			checkSum;		// myId + frameNumber + nBlobs	?  that's not ideal but all I can think of at the moment.
 
-
+	computerVisionPacket() {
+		myId = 0;
+		frameNumber = 0;
+		width = 0;
+		height = 0;
+		nBlobs = 0;
+		checkSum = 0;
+	}
+}	;
 
 #endif
