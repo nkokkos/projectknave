868c5896a1456160aaea5a8e135db37993c9fef9
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-15 01:08:12 +0000

    added support for daito

--- trunk/development/centralApp/src/testApp.cpp
+++ trunk/development/centralApp/src/testApp.cpp
@@ -1,55 +1,43 @@
 #include "testApp.h"
 
-
-
-
-
 //--------------------------------------------------------------
 void testApp::setup(){
-
 	ofBackground(90, 90, 90);
 
-	
+	ofxDaito::setup("settings/daito.xml");
+
 	building.loadImage("buildingRefrences/building.jpg");
 	mask.loadImage("buildingRefrences/mask_half.png");
-	
-	
+
 	XML.loadFile("settings/mainAppSettings.xml");
-	bUseNetworking = XML.getValue("mainApp:useCvNetworking", 0);	
+	bUseNetworking = XML.getValue("mainApp:useCvNetworking", 0);
 	CVM.id = XML.getValue("mainApp:id", 0);
-	
-	
+
 	SM.setup();
 	SM.gotoScene(HAND_SCENE);
-	
-	
+
 	RM.setup();
-	
+
 	drawMode = DRAW_SCENE;
-	
-	CVM.setupNonCV();	// this order is all wonky now. 
-	
-	
-	
+
+	CVM.setupNonCV();	// this order is all wonky now.
+
 	//MRM
 	nScreens = 6;
 	MRM.allocateForNScreens(nScreens, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT);
 	MRM.loadFromXml("settings/fboSettings.xml");
-	
-	float ratio = .2;//(float)OFFSCREEN_HEIGHT/OFFSCREEN_WIDTH;
-	
+
+	float ratio = .2;
+
 	guiIn   = ofRectangle(0, 0, OFFSCREEN_WIDTH*ratio, OFFSCREEN_HEIGHT*ratio);
-    guiOut  = ofRectangle(guiIn.x + guiIn.width + 30, 40, 500, 178);
-	
-	
-	
+	guiOut  = ofRectangle(guiIn.x + guiIn.width + 30, 40, 500, 178);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	
-	
-	// if we are using networking, send / recv network data: 
+
+
+	// if we are using networking, send / recv network data:
 	if (bUseNetworking == true){
 		if (CVM.id == 0){
 			// TODO: check the isFrameNew here...
@@ -60,35 +48,35 @@ void testApp::update(){
 			CVM.receiveFromNetwork();
 		}
 	}
-	
-	
-	
+
+
+
 	if (bUseNetworking){
 		if (CVM.id == 0){
 			CVM.update();
 			//CVM.receiveFromNetwork();
-		}	
+		}
 	} else {
 		CVM.update();
 	}
-	
-	
-	
-	//nothing in RM update at the moment. 
+
+
+
+	//nothing in RM update at the moment.
 	//RM.update();
-	
-	
-	
+
+
+
 	if ((drawMode == DRAW_SCENE)){
 		SM.passInPacket(CVM.packet);
 		SM.update();
 	}
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	
+
 	if (bUseNetworking == true){
 		if (CVM.id == 0){
 			ofBackground(255,0,255);
@@ -96,26 +84,26 @@ void testApp::draw(){
 			return;
 		}
 	}
-	
-	
-	
+
+
+
 	ofSetColor(0, 0, 0);
 	string info = "	FPS: "+ofToString(ofGetFrameRate());
 	info += "\n		left / right key to change draw";
 	info += "\n		up / down to change scenes.";
 	info += "\n		Scene: "+ofToString(SM.currentScene)+"/"+ofToString(SM.numScenes);
-	
-	
-	
-	
-	
+
+
+
+
+
 	if (drawMode == DRAW_CV){
 		CVM.draw();
-		
-	} 
-	
+
+	}
+
 	else if (drawMode == DRAW_SCENE) {
-		
+
 		// ---- do off screen rendering
 		ofSetColor(255, 255, 255, 255);
 		MRM.startOffscreenDraw();
@@ -124,9 +112,9 @@ void testApp::draw(){
 		mask.draw(0, 0, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT);
 
 		MRM.endOffscreenDraw();
-		
-	
-		
+
+
+
 		// ---- now draw the screens
 		glPushMatrix();
 		glTranslatef(0, 0, 0);
@@ -135,47 +123,47 @@ void testApp::draw(){
 			MRM.drawScreen(i);
 		}
 		glPopMatrix();
-		
+
 		// ---- draw the gui utils
 		ofSetColor(255, 255, 255, 255);
 		MRM.drawInputDiagnostically(guiIn.x, guiIn.y, guiIn.width, guiIn.height);
 		MRM.drawOutputDiagnostically(guiOut.x, guiOut.y, guiOut.width, guiOut.height);
-		
-		
+
+
 		/*	RM.swapInFBO();
 		 SM.draw();
-		 
+
 		 ofEnableAlphaBlending();
 		 ofSetColor(255,255,255, 210);
-		 
+
 		 mask.draw(0, 0, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT);
-		 
+
 		 // move outside fbo can see it better
 		 //ofSetColor(255,255,255);
 		 //SM.drawTop();
-		 
+
 		 RM.swapOutFBO();
 		 ofSetColor(255,255,255);
 		 RM.drawForPreview();
-		 */	
+		 */
 	}
-	
+
 	if(drawMode==DRAW_SCENE) {
-		
+
 		ofSetColor(255,255,255);
 		//	SM.drawTop();
 	}
-	
+
 	ofDrawBitmapString(info, 20, 20);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-	
+
 	SM.keyPressed(key);
-	
+
 	switch (key){
-			
+
 		case 'f':
 			ofToggleFullscreen();
 			break;
@@ -185,29 +173,29 @@ void testApp::keyPressed(int key){
 			break;
 		case OF_KEY_LEFT:
 			drawMode --;
-			if (drawMode < 0) drawMode += 3;	
+			if (drawMode < 0) drawMode += 3;
 			break;
-			
+
 		case OF_KEY_UP:
 			SM.nextScene();
 			break;
 		case OF_KEY_DOWN:
 			SM.prevScene();
 			break;
-			
-			
+
+
 		case 's':
 			MRM.saveToXml();
 			break;
-			
+
 		case 'r':
 			MRM.reloadFromXml();
 			break;
-			
+
 		case 'c':
 			MRM.resetCoordinates();
 			break;
-			
+
 	}
 }
 
@@ -219,46 +207,46 @@ void testApp::keyReleased(int key){
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ) {
 	ofPoint pos = RM.getPointInPreview(x, y);
-	SM.mouseMoved(pos.x, pos.y);	
+	SM.mouseMoved(pos.x, pos.y);
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button) {
 	ofPoint pos = RM.getPointInPreview(x, y);
 	SM.mouseDragged(pos.x, pos.y, button);
-	
+
 	if (drawMode == DRAW_CV || bUseNetworking){
 		CVM.mouseDragged(x, y, button);
 	}
-	
-	
+
+
 	MRM.mouseDragInputPoint(guiIn, ofPoint(x, y));
 	MRM.mouseDragOutputPoint(guiOut, ofPoint( x, y));
-	
-	
+
+
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button) {
 	ofPoint pos = RM.getPointInPreview(x, y);
 	SM.mousePressed(pos.x, pos.y, button);
-	
+
 	if (drawMode == DRAW_CV || bUseNetworking){
 		CVM.mousePressed(x, y, button);
 	}
-	
-	
+
+
 	if( !MRM.mouseSelectInputPoint(guiIn, ofPoint(x, y)) ){
         MRM.mouseSelectOutputPoint(guiOut, ofPoint( x,  y));
     }
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button) {
 	ofPoint pos = RM.getPointInPreview(x, y);
 	SM.mouseReleased(pos.x, pos.y, button);
-	
+
 	if (drawMode == DRAW_CV || bUseNetworking){
 		CVM.mouseReleased();
 	}
@@ -266,7 +254,7 @@ void testApp::mouseReleased(int x, int y, int button) {
 
 //--------------------------------------------------------------
 void testApp::resized(int w, int h){
-	
+
 }
 
 
