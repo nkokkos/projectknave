3bb22d165f40ca246d44b0b3e3180f067c03c877
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-16 02:59:22 +0000

    needs math.h on win gcc

--- trunk/development/centralApp/src/scenes/StarScene/particle/ImprovedNoise.h
+++ trunk/development/centralApp/src/scenes/StarScene/particle/ImprovedNoise.h
@@ -3,18 +3,19 @@
 #define IMPROVED_NOISE_
 
 #include <stdlib.h>
+#include <math.h>
 
 class ImprovedNoise {
 
 public:
 	ImprovedNoise();
-	
+
 	double noise(double x, double y, double z);
-	
+
 	double fade(double t);
 	double lerp(double t, double a, double b);
 	double grad(int hash, double x, double y, double z);
-	
+
 	//int * p;
 	int p[512];
 };
