04f02a958d933fc8b831bd20b619b1462a174d74
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-16 05:19:09 +0000

    thicker lines

--- development/centralApp/src/scenes/HandScene/buildingShape.h
+++ development/centralApp/src/scenes/HandScene/buildingShape.h
@@ -12,47 +12,49 @@
 #include "ofMain.h"
 #include "ofxDisplayList.h"
 
+#define BUILDING_HIGHLIGHT_SIZE 8
+
 class buildingShape {
-	
-public: 
+
+public:
 
 	int type;
-	
+
 	float animator;			// between 0 and 1
 	float direction;		// -1 or 1
-	
+
 	vector <ofPoint >  trail;
-	
+
 	ofPoint centroid;
 	ofRectangle boundingRect;
 	int nPts;
 	ofPoint * pts;
-	
+
 	float length;
 	float energy;
-	
+
 	ofImage * spot;
-	
+
 	bool	bHasPeakedWhileTouched;
 	float	peakEnergy;
-	
+
 	bool	bPeakedThisFrame;
 	float	peakMakeStuffSick;
 	bool	bAnyInsideMeFromLastFrame;
 	int		nFramesSinceTrigger;
-	
+
 	buildingShape();
 	void draw();
 	void update();
 	void checkInside(float x, float y, ofPoint colorMe);
-	
+
 	void setupList();
 	bool			bSetupList;
 	ofxDisplayList DL;
-	
+
 	ofPoint			myColor;
 	ofPoint			myColorTarget;
-	
-	
-	
+
+
+
 };
