9870b37498441c804680657f8eea15f07a4a7b30
kylecimcdonald <kylecimcdonald@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-16 04:46:54 +0000

    yellow fix

--- development/centralApp/src/render/FBO/megaRenderManager.cpp
+++ development/centralApp/src/render/FBO/megaRenderManager.cpp
@@ -5,15 +5,15 @@
 //---------------------------------------------------------------------------
 megaRenderManager::megaRenderManager(){
     bInputSelected = false;
-	
+
     activeScreenIn = -1;
     activePointIn  = -1;
-	
+
     activeScreenOut = -1;
     activePointOut  = -1;
-	
+
     xmlFile = "";
-	
+
     nScreens = 0;
 }
 
@@ -25,36 +25,36 @@ void megaRenderManager::allocateForNScreens(int numScreens, int renderWidth, int
     }
     width   = renderWidth;
     height  = renderHeight;
-	
+
     screenWidth  = (float)width / (float)numScreens;
     screenHeight = height;
-	
+
     myOffscreenTexture.allocate(renderWidth, renderHeight, true);
 
 	myOffscreenTexture.setClearColor(0,0, 0,255);
-	
+
     nScreens            = numScreens;
     outputPositions     = new ofPoint*[nScreens];
     inputPositions      = new ofPoint*[nScreens];
-	
+
     float w_d_3 = (float)width / (float)nScreens;
     float inner_d_3 = 1.0f / (float)nScreens;
     for (int i = 0; i < nScreens; i++){
-		
+
         inputPositions[i]  = new ofPoint[4];
         outputPositions[i] = new ofPoint[4];
-		
+
         //-----------------------------------------
         outputPositions[i][0].set(w_d_3*(i),0,0);
         outputPositions[i][1].set(w_d_3*(i+1),0,0);
         outputPositions[i][2].set(w_d_3*(i+1),height,0);
         outputPositions[i][3].set(w_d_3*(i),height,0);
-		
+
         inputPositions[i][0].set(inner_d_3*(i),0,0);
         inputPositions[i][1].set(inner_d_3*(i+1),0,0);
         inputPositions[i][2].set(inner_d_3*(i+1),1,0);
         inputPositions[i][3].set(inner_d_3*(i),1,0);
-		
+
 		//
 		//        printf("input is \n");
 		//        printf("(%f %f)          ", inputPositions[i][0].x, inputPositions[i][0].y);
@@ -73,7 +73,7 @@ void megaRenderManager::allocateForNScreens(int numScreens, int renderWidth, int
 //---------------------------------------------------------------------------
 void megaRenderManager::startOffscreenDraw(){
     if( nScreens <= 0 ) return;
-	
+
     myOffscreenTexture.swapIn();
     myOffscreenTexture.setupScreenForMe();
 }
@@ -81,7 +81,7 @@ void megaRenderManager::startOffscreenDraw(){
 //---------------------------------------------------------------------------
 void megaRenderManager::endOffscreenDraw(){
     if( nScreens <= 0 ) return;
-	
+
     myOffscreenTexture.swapOut();
     myOffscreenTexture.setupScreenForThem();
 }
@@ -89,9 +89,9 @@ void megaRenderManager::endOffscreenDraw(){
 //---------------------------------------------------------------------------
 void megaRenderManager::drawInputDiagnostically(float x, float y, float w, float h){
 	if( nScreens <= 0 ) return;
-	
+
     myOffscreenTexture.draw(x, y, w,h);
-	
+
     for (int i = 0; i < nScreens; i++){
         ofSetColor(0x33DD44);
         ofNoFill();
@@ -100,38 +100,38 @@ void megaRenderManager::drawInputDiagnostically(float x, float y, float w, float
             ofVertex( inputPositions[i][j].x * w + x, inputPositions[i][j].y * h + y);
         }
         ofEndShape(true);
-		
+
         ofFill();
         ofSetColor(0x3344FF);
         for (int j = 0; j < 4; j++){
             ofCircle(  inputPositions[i][j].x * w + x, inputPositions[i][j].y * h + y,3);
         }
     }
-	
+
 }
 
 //---------------------------------------------------------------------------
 void megaRenderManager::drawOutputDiagnostically(float x, float y, float w, float h){
     if( nScreens <= 0 ) return;
-	
+
     glPushMatrix();
     glTranslatef(x, y, 0);
-	
+
     ofNoFill();
 	ofSetColor(0x000000);
 	ofRect(0, 0, w, h);
     ofFill();
-	
+
     for (int i = 0; i < nScreens; i++){
-		
+
         glPushMatrix();
-		
+
 		ofSetColor(255, 255, 255, 255);
 		float wScale = ( w / screenWidth ) / nScreens;
 		float hScale = h / screenHeight;
 		glScalef(wScale, hScale, 1);
 		drawScreen(i);
-		
+
 		ofSetColor(0x4444CC);
 		ofNoFill();
 		ofBeginShape();
@@ -139,45 +139,45 @@ void megaRenderManager::drawOutputDiagnostically(float x, float y, float w, floa
 			ofVertex( outputPositions[i][j].x  , outputPositions[i][j].y );
 		}
 		ofEndShape(true);
-		
+
 		ofFill();
 		ofSetColor(0xFF3333);
 		for (int j = 0; j < 4; j++){
 			ofCircle(  outputPositions[i][j].x , outputPositions[i][j].y, 5 );
 		}
-		
+
         glPopMatrix();
     }
-	
+
     glPopMatrix();
-	
+
 }
 
 //---------------------------------------------------------------------------
 void megaRenderManager::drawScreen(int nScreen, bool debugMode){
-	
+
 	if( nScreens <= 0 ) return;
-	
+
 	if(debugMode) {
-		
+
 		float ratio = (float)OFFSCREEN_HEIGHT / (float)OFFSCREEN_WIDTH;
-		
+
 		float width = ofGetWidth();
 		float height = ratio * width;
 		float diff = ofGetHeight() - height;
-		if (diff < 0) diff = 0;	
-		
+		if (diff < 0) diff = 0;
+
 		myOffscreenTexture.setPoints(inputPositions[nScreen], outputPositions[nScreen]);
 		myOffscreenTexture.draw(0,diff,width, height);
 	}
 	else {
 		myOffscreenTexture.setPoints(inputPositions[nScreen], outputPositions[nScreen]);
 		myOffscreenTexture.draw();
-		
-		ofSetColor(255, 255, 0);
+
+		ofSetColor(255, 255, 255);
 		float tx = inputPositions[nScreen][1].x * 600;
 		float ty = inputPositions[nScreen][0].y + 600;
-		
+
 		//ofCircle(  outputPositions[i][j].x , outputPositions[i][j].y, 5 );
 
 		//ofDrawBitmapString(ofToString(nScreen), tx, ty);
@@ -188,16 +188,16 @@ void megaRenderManager::drawScreen(int nScreen, bool debugMode){
 //---------------------------------------------------------------------------
 void megaRenderManager::resetCoordinates(){
     if( nScreens <= 0)return;
-	
+
     float inner_d_3 = 1.0f / (float)nScreens;
     for (int i = 0; i < nScreens; i++){
-		
+
         //-----------------------------------------
         outputPositions[i][0].set(screenWidth*(i),0,0);
         outputPositions[i][1].set(screenWidth*(i+1),0,0);
         outputPositions[i][2].set(screenWidth*(i+1),height,0);
         outputPositions[i][3].set(screenWidth*(i),height,0);
-		
+
         inputPositions[i][0].set(inner_d_3*(i),0,0);
         inputPositions[i][1].set(inner_d_3*(i+1),0,0);
         inputPositions[i][2].set(inner_d_3*(i+1),1,0);
@@ -215,46 +215,46 @@ void megaRenderManager::loadFromXml(string xmlFileName){
 //---------------------------------------------------------------------------
 void megaRenderManager::reloadFromXml(){
     if(xmlFile == "")return;
-	
+
     for(int i = 0; i < nScreens; i++){
-		
+
         float inner_d_3 = 1.0 / nScreens;
-		
+
         if( xml.pushTag("QUAD_IN_"+ofToString(i)) ){
-			
+
 			float inner_d_3 = 1.0 / nScreens;
-			
+
 			inputPositions[i][0].x  = xml.getValue("pt0:x", inner_d_3*(i));
 			inputPositions[i][0].y  = xml.getValue("pt0:y", 0.0f);
-			
+
 			inputPositions[i][1].x  = xml.getValue("pt1:x", inner_d_3*(i+1));
 			inputPositions[i][1].y  = xml.getValue("pt1:y", 0.0f);
-			
+
 			inputPositions[i][2].x  = xml.getValue("pt2:x", inner_d_3*(i+1));
 			inputPositions[i][2].y  = xml.getValue("pt2:y", 1.0f);
-			
+
 			inputPositions[i][3].x  = xml.getValue("pt3:x", inner_d_3*(i));
 			inputPositions[i][3].y  = xml.getValue("pt3:y", 1.0f);
-			
+
             xml.popTag();
         }
         if( xml.pushTag("QUAD_OUT_"+ofToString(i)) ){
-			
+
 			outputPositions[i][0].x  = xml.getValue("pt0:x", screenWidth*(i));
 			outputPositions[i][0].y  = xml.getValue("pt0:y", 0);
-			
+
 			outputPositions[i][1].x  = xml.getValue("pt1:x", screenWidth*(i+1));
 			outputPositions[i][1].y  = xml.getValue("pt1:y", 0);
-			
+
 			outputPositions[i][2].x  = xml.getValue("pt2:x", screenWidth*(i+1));
 			outputPositions[i][2].y  = xml.getValue("pt2:y", height);
-			
+
 			outputPositions[i][3].x  = xml.getValue("pt3:x", screenWidth*(i));
 			outputPositions[i][3].y  = xml.getValue("pt3:y", height);
-			
+
             xml.popTag();
         }
-		
+
     }
 }
 
@@ -269,70 +269,70 @@ void megaRenderManager::saveToXml(){
 
 void megaRenderManager::saveToXml(string xmlFileName){
     if(xmlFileName == "")return;
-	
+
     //clear out the file
     xml.clear();
-	
+
     for(int i = 0; i < nScreens; i++){
-		
+
         float inner_d_3 = 1.0 / nScreens;
-		
+
         xml.addTag("QUAD_IN_"+ofToString(i) );
         if( xml.pushTag("QUAD_IN_"+ofToString(i)) ){
-			
+
 			float inner_d_3 = 1.0 / nScreens;
-			
+
 			xml.setValue("pt0:x", inputPositions[i][0].x);
 			xml.setValue("pt0:y", inputPositions[i][0].y);
-			
+
 			xml.setValue("pt1:x", inputPositions[i][1].x);
 			xml.setValue("pt1:y", inputPositions[i][1].y);
-			
+
 			xml.setValue("pt2:x", inputPositions[i][2].x);
 			xml.setValue("pt2:y", inputPositions[i][2].y);
-			
+
 			xml.setValue("pt3:x",  inputPositions[i][3].x);
 			xml.setValue("pt3:y",  inputPositions[i][3].y);
-			
+
             xml.popTag();
         }
-		
+
         xml.addTag("QUAD_OUT_"+ofToString(i) );
         if( xml.pushTag("QUAD_OUT_"+ofToString(i)) ){
-			
+
 			xml.setValue("pt0:x", outputPositions[i][0].x);
 			xml.setValue("pt0:y", outputPositions[i][0].y);
-			
+
 			xml.setValue("pt1:x", outputPositions[i][1].x);
 			xml.setValue("pt1:y", outputPositions[i][1].y);
-			
+
 			xml.setValue("pt2:x", outputPositions[i][2].x);
 			xml.setValue("pt2:y", outputPositions[i][2].y);
-			
+
 			xml.setValue("pt3:x", outputPositions[i][3].x);
 			xml.setValue("pt3:y", outputPositions[i][3].y);
-			
+
             xml.popTag();
         }
     }
-	
+
     xml.saveFile(xmlFileName);
 }
 
 //---------------------------------------------------------------------------
 bool megaRenderManager::mouseSelectInputPoint(ofRectangle drawRect, ofPoint mousePoint, float clickRadius){
     if( nScreens <= 0 ) return false;
-	
+
     //this is not the best way
     activeScreenIn      = -1;
     activePointIn       = -1;
     activeScreenOut     = -1;
     activePointOut      = -1;
-	
+
     float smallestDist  = 999999;
-	
+
     bool bFound = false;
-	
+
     for (int i = 0; i < nScreens; i++){
         for (int j = 0; j < 4; j++){
             ofPoint inputPt;
@@ -345,97 +345,97 @@ bool megaRenderManager::mouseSelectInputPoint(ofRectangle drawRect, ofPoint mous
                 activeScreenIn = i;
                 activePointIn  = j;
                 smallestDist = len;
-				
+
                 bFound = true;
             }
         }
     }
-	
+
     return bFound;
 }
 
 //---------------------------------------------------------------------------
 bool megaRenderManager::mouseDragInputPoint(ofRectangle drawRect, ofPoint mousePoint){
     if( nScreens <= 0 ) return false;
-	
+
     if (activeScreenIn > -1 && activePointIn > -1){
-		
+
         float xpct = (mousePoint.x - drawRect.x) / drawRect.width;
         float ypct = (mousePoint.y - drawRect.y) / drawRect.height;
         inputPositions[activeScreenIn][activePointIn].x = xpct;
         inputPositions[activeScreenIn][activePointIn].y = ypct;
-		
+
         return true;
     }
-	
+
     return false;
 }
 
 //---------------------------------------------------------------------------
 bool megaRenderManager::mouseSelectOutputPoint(ofRectangle drawRect, ofPoint mousePoint, float clickRadius){
     if( nScreens <= 0 ) return false;
-	
+
     //this is not the best way
     activeScreenIn      = -1;
     activePointIn       = -1;
     activeScreenOut     = -1;
     activePointOut      = -1;
-	
+
     float smallestDist  = 999999;
-	
+
     bool bFound = false;
-	
+
     for (int i = 0; i < nScreens; i++){
         for (int j = 0; j < 4; j++){
             ofPoint outputPt;
-			
+
             //lets get the screen width and height at our scale
             outputPt.x = outputPositions[i][j].x;// * drawRect.width) + drawRect.x;
             outputPt.y = outputPositions[i][j].y;// * drawRect.height) + drawRect.y;
             outputPt.z = 0;
-			
+
             outputPt.x *= ( (float)drawRect.width  / screenWidth )/nScreens;
             outputPt.y *= (float)drawRect.height / screenHeight;
-			
+
             outputPt.x += drawRect.x;
             outputPt.y += drawRect.y;
-			
+
             //printf("outputPt  is %f %f \n", outputPt.x, outputPt.y);
-			
+
             float len = sqrt( (outputPt.x - mousePoint.x) * (outputPt.x - mousePoint.x) +
 							 (outputPt.y - mousePoint.y) * (outputPt.y - mousePoint.y));
             if (len < clickRadius && len < smallestDist){
                 activeScreenOut = i;
                 activePointOut = j;
                 smallestDist = len;
-				
+
                 //printf("setting point %i of screen %i \n", activePointOut, activeScreenOut);
-				
+
                 bFound = true;
             }
         }
     }
     return bFound;
-	
+
 }
 
 //---------------------------------------------------------------------------
 bool megaRenderManager::mouseDragOutputPoint(ofRectangle drawRect, ofPoint mousePoint){
     if( nScreens <= 0 ) return false;
-	
+
     if (activeScreenOut > -1 && activePointOut > -1){
-		
+
         float xpct = (mousePoint.x - drawRect.x) / (drawRect.width / nScreens);
         float ypct = (mousePoint.y - drawRect.y) / drawRect.height;
-		
+
         float outPtX = xpct * screenWidth;
         float outPtY = ypct * screenHeight;
-		
+
         outputPositions[activeScreenOut][activePointOut].x = outPtX;
         outputPositions[activeScreenOut][activePointOut].y = outPtY;
-		
+
         //printf("point - %i  of screen %i setting output position to %f %f \n",activePointOut,  activeScreenOut, outputPositions[activeScreenOut][activePointOut].x, outputPositions[activeScreenOut][activePointOut].y);
-		
+
         return true;
     }
     return false;
