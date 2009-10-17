dc450f50367ec97eedf57ed4dd60aba835e30761
vanderlin <vanderlin@d8efe63e-b2cf-11de-8feb-81d43c80ca5a>
2009-10-17 02:23:24 +0000

    add mouse world points

--- trunk/development/centralApp/src/scenes/TreeScene/TreeScene.cpp
+++ trunk/development/centralApp/src/scenes/TreeScene/TreeScene.cpp
@@ -2,16 +2,34 @@
 
 // ------------------------------------------
 void TreeScene::setup() {
-	curStem = 0;
 	
+	
+	theDot.loadImage("sceneAssets/monsters/dot.png");
+	
+	
+	// ------------ Control Panel For Trees
+	panel.setup("Tree Scene", 700, 10, 300, 750);
+	panel.addPanel("Triggers", 1, false);
+	
+	panel.setWhichPanel("Triggers");
+	panel.addSlider("Tree Delay", "TREE_DELAY", 1.3, 0.0, 10.0, false);
+	panel.addSlider("Bottom Offset", "BOTTOM_OFFSET", 100, 0.0, 500.0, false);
+	
+	
+	// ------------ The Ferr Contour
 	building.setupBuilding("buildingRefrences/buidlingFiles/treeFerryContour.xml");
 	
 	
-	//for(int i=0; i<3; i++) {
-	//		leaves.push_back(ofxSvg());
-	//		leaves.back().loadFile("sceneAssets/trees/Fern_Leaf_"+ofToString(i)+".svg");
-	//	}
 	
+	// ------------ Load some SVG Files for the Ferns 
+	RandomFern tempFern;
+	tempFern.addLeaf("sceneAssets/trees/Fern_Leaf_001.svg");
+	tempFern.addLeaf("sceneAssets/trees/Fern_Leaf_002.svg");
+	tempFern.addLeaf("sceneAssets/trees/Fern_Leaf_003.svg");
+	
+	
+	
+	// ------------ Settins 
 	W = 1.0;	
 	H = 1.0;
 	
@@ -20,7 +38,7 @@ void TreeScene::setup() {
 	minSpawnToGrowH		= 120;
 	load();
 	
-	
+	// ------------ Get some Flocling butterflys in
 	for (int i = 0; i < 60; i++){
 		ButterFly bf;
 		bf.setInitialCondition(ofRandom(200, 500), ofRandom(0, OFFSCREEN_HEIGHT), 0,0);
@@ -28,31 +46,31 @@ void TreeScene::setup() {
 		butterflys.push_back(bf);
 	}
 	
-	theDot.loadImage("sceneAssets/monsters/dot.png");
-	
 }
 
-// ------------------------------------------
+//--------------------------------------------------------------
+void TreeScene::keyPressed(int key) {
+	// keys for the ferry
+	building.keyPressed(key);	
+}
 
-void TreeScene::mousePressed(int x, int y, int button) {
-	stems[curStem].initStem(x, y);
-	curStem ++;
-	curStem %= MAX_STEMS;
+//--------------------------------------------------------------
+void TreeScene::mouseDragged(int wx, int wy, int x, int y, int button){
+	panel.mouseDragged(wx, wy, button);
+}
+
+//--------------------------------------------------------------
+void TreeScene::mousePressed(int wx, int wy, int x, int y, int button){
+	panel.mousePressed(wx, wy, button);
 	
 	// events for the ferry building
 	building.mousePressed(x, y, button);
 }
 
-
 //--------------------------------------------------------------
-void TreeScene::keyPressed(int key) {
-	
-	// keys for the ferry
-	building.keyPressed(key);
-	
+void TreeScene::mouseReleased(int wx, int wy, int x, int y, int button){
+	panel.mouseReleased();
 }
-
-
 
 
 
@@ -125,6 +143,38 @@ void TreeScene::updateFlocking() {
 // ------------------------------------------
 void TreeScene::update() {
 	
+	panel.update();
+	
+	
+	
+	
+	// --------------------- add some fern
+	if((int)ofRandom(0, 30) == 10) {
+		if(ferns.size() <= 10) {	// just 10 for now
+			ferns.push_back(RandomFern());
+			printf("-- new fern --\n");
+			
+		}
+	}
+	
+	// --------------------- Tree Blobs
+	for(int i=0; i<treeBlobs.size(); i++) {
+		treeBlobs[i].age = (float)(ofGetElapsedTimef()-treeBlobs[i].initTime);	
+		if(treeBlobs[i].age > panel.getValueF("TREE_DELAY") && !treeBlobs[i].bAlive) {
+			treeBlobs[i].bAlive = true;	
+			
+			// time to grow a tree
+			printf("--- time to grow a tree:%i---\n", treeBlobs[i].id);
+			trees.push_back(MagicTree());
+			
+			trees.back().initTree(0, 0, (int)ofRandom(20, 50));	// <--- i need a init pos
+			trees.back().img = &theDot;
+			trees.back().id = treeBlobs[i].id;
+			
+		}
+		
+		
+	}
 	
 	
 	// --------------------- Tree People
@@ -154,7 +204,7 @@ void TreeScene::update() {
 		for(int j=0; j<trees.size(); j++) {
 			if(lookID == trees[j].id) {
 				trees[j].treeBase = tracker->blobs[i].centroid * TREE_SCALE;
-				trees[j].frameAge = tracker->blobs[i].frameAge;
+				trees[j].frameAge =0;// tracker->blobs[i].frameAge;
 				
 			}
 		}
@@ -208,22 +258,52 @@ void TreeScene::update() {
 		printf("got my first packet - %i\n", packet.frameNumber);
 		bGotMyFirstPacket = true;
 		
-		W = packet.width * TREE_SCALE;
-		H = 100 + (packet.height * TREE_SCALE);
 	}
 	
+	W = packet.width * TREE_SCALE;
+	H = panel.getValueF("BOTTOM_OFFSET") + (packet.height * TREE_SCALE);
 	
-	
+}
 
+// ---------------------------------------------------------
+void TreeScene::drawTop() {
+	panel.draw();
 }
 
 // ---------------------------------------------------------
 void TreeScene::draw() {
 	
+	
+	
+	
 	ofEnableAlphaBlending();
 	glPushMatrix();
 	//glTranslatef(mouseX, mouseY, 0);
 	glTranslatef(((OFFSCREEN_WIDTH - W)/2), (OFFSCREEN_HEIGHT-H), 0);
+	
+	
+	// draw the ferns
+	for(int i=0; i<ferns.size(); i++) {
+		ofPushStyle();
+		glPushMatrix();
+		glTranslatef(ferns[i].pos.x, ferns[i].pos.y, 0);
+		ferns[i].draw(1.0);
+		glPopMatrix();
+		ofPopStyle();
+	}
+	
+	// The Tree Blobs
+	for(int i=0; i<treeBlobs.size(); i++) {
+		ofFill();
+		ofSetColor(200, 40, 255);
+		ofCircle(i*85, 0, 40);
+		ofSetColor(255, 255, 255);
+		ofDrawBitmapString("id: "+ofToString(treeBlobs[i].id) +"\n"+ ofToString(treeBlobs[i].age), i*78, 10);
+	}
+	
+	
+	
+	
 	
 	for(int i=0; i<packet.nBlobs; i++) {
 		ofSetColor(255, i*20, 255);
@@ -269,29 +349,44 @@ void TreeScene::draw() {
 // --------------------------------------------------------- blob events
 void TreeScene::blobOn( int x, int y, int bid, int order ) {
 	
-	printf("new tree  - %i\n", bid);
-	ofxCvBlob blober = tracker->getById(bid);
+	ofCvTrackedBlob * blober = &tracker->getById(bid);
 	
 	// if i am hole make a shape instead
-	if(blober.hole) {
+	if(blober->hole) {
 		
 	}
 	
 	// else make a Tree
 	else {
-		//if(blober.boundingRect.width >= minSpawnToGrowW || blober.boundingRect.height >= minSpawnToGrowH) {
-	//	treePeople.push_back(TreePerson());
-	//	treePeople.back().init( tracker->getById(bid) );
-	//	treePeople.back().tree.img = &theDot;
-		//}
-		trees.push_back(MagicTree());
 		
-		float tx = x * TREE_SCALE;
-		float ty = y * TREE_SCALE;
-		trees.back().initTree(tx, ty, 10);
-		trees.back().img = &theDot;
-		trees.back().id = bid;
-		trees.back().frameAge = tracker->getById(bid).frameAge;
+		
+		treeBlobs.push_back(TreeBlob());
+		treeBlobs.back().id = bid;
+		treeBlobs.back().age = 0;
+		treeBlobs.back().initTime = ofGetElapsedTimef();
+		
+		
+		//if(blober.boundingRect.width >= minSpawnToGrowW || blober.boundingRect.height >= minSpawnToGrowH) {
+		//	treePeople.push_back(TreePerson());
+		//	treePeople.back().init( tracker->getById(bid) );
+		//	treePeople.back().tree.img = &theDot;
+		//}
+		/*
+		 printf("-- id:%i, frameAge:%i\n", bid, tracker->getById(bid).frameAge);
+		 if(blober->frameAge > 50) {
+		 trees.push_back(MagicTree());
+		 
+		 float tx = x * TREE_SCALE;
+		 float ty = y * TREE_SCALE;
+		 trees.back().initTree(tx, ty, 10);
+		 trees.back().img = &theDot;
+		 trees.back().id = bid;
+		 trees.back().frameAge = blober->frameAge;
+		 
+		 printf("new tree  - %i\n", bid);
+		 
+		 }
+		 */
 	}
 	
 	
@@ -301,27 +396,26 @@ void TreeScene::blobOn( int x, int y, int bid, int order ) {
 // ---------------------------------------------------------
 void TreeScene::blobMoved( int x, int y, int bid, int order ) {
 	
-	for(int i=treePeople.size()-1; i>=0; i--) {
-		if(treePeople[i].id == bid) {
-			
-			// tell daito that the tree just moved
-			ofxCvBlob blober = tracker->getById(bid);
-			
-		}
-	}
-	
 }
 
 // ---------------------------------------------------------
 void TreeScene::blobOff( int x, int y, int bid, int order ) {	
 	
-	printf("tree off - %i\n", bid);
 	for(int i=trees.size()-1; i>=0; i--) {
 		if(trees[i].id == bid) {
 			
 			trees[i].id = -1;
 			trees[i].bNoBlobAnymore = true;
-		
+			printf("tree off - %i\n", bid);
+			
+		}
+	}
+	
+	// clean up the tree blobs
+	for(int i=treeBlobs.size()-1; i>=0; i--) {
+		if(treeBlobs[i].id == bid) {
+			treeBlobs.erase(treeBlobs.begin() + i);
+			printf("--- tree blob removed [%i] -- \n", bid);
 		}
 	}
 }
